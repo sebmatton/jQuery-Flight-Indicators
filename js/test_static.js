781a8a7089d7023a074b462c834fb5857b648640
Sid Kumar <sid@flyarcher.com>
2020-10-06 16:27:02 -0700

    Add test

---
+++ js/test_static.js
@@ -0,0 +1,10 @@
+function countMyself() {
+    // Check to see if the counter has been initialized
+    if ( typeof countMyself.counter == 'undefined' ) {
+        // It has not... perform the initialization
+        countMyself.counter = 0;
+    }
+
+    // Do something stupid to indicate the value
+    alert(++countMyself.counter);
+}
